b1e91fd1794f18fc11961e2b1c7d7f4e16ea87ac
ajansamel <50684981+ajansamel@users.noreply.github.com>
2020-04-06 13:42:02 -1000

    Adding working code
    Adding working code that takes a couple of command line parameters. Still need to develop so that we can run on any sharepoint server and any documents folder.

---
+++ sharepoint_webscraping/sharepoint_webscraping.py
@@ -0,0 +1,61 @@
+from bs4 import BeautifulSoup
+import requests
+from requests.auth import HTTPBasicAuth
+import urllib.request
+import urllib
+import os
+import re
+import json
+from pathlib import Path
+import sys
+import getopt
+
+sharepointUri = 'http://sharepoint'
+baseUri = urllib.parse.urljoin(sharepointUri,'operations/solar/Shared%20Documents/Forms/AllItems.aspx')
+basicAuth = HTTPBasicAuth('HSI\\ajans','cH3wyM0ck!ngb!rd')
+dlRoot = 'C:/TEMP' #os.path.join(Path.home(),'HSI/AEC Alternative Energy - Documents')
+
+try:
+  opts, args = getopt.getopt(sys.argv[1:],"hu:d:",["url=","dest="])
+except getopt.GetoptError:
+  print('sharepoint_webscraping.py -u <url> -d <destpath>')
+  sys.exit(2)
+for opt, arg in opts:
+  if opt == '-h':
+     print('sharepoint_webscraping.py -u <url> -d <destpath>')
+     sys.exit()
+  elif opt in ("-u", "--url"):
+     baseUri = arg
+  elif opt in ("-d", "--dest"):
+     dlRoot = arg
+
+def download_file(url_path, basic_auth):
+    source = requests.get(urllib.parse.urljoin(sharepointUri, url_path), auth=basic_auth)
+    filename = os.path.join(dlRoot, urllib.parse.unquote(url_path))
+    print(filename)
+    os.makedirs(filename.replace(filename.split('/')[-1],''), exist_ok=True)
+    if not os.path.isfile(filename):
+        with open(filename, 'wb') as out_file:
+            out_file.write(source.content)
+        print("Download Complete!")
+
+def download_folder(url_path, basic_auth):
+    source = requests.get(url_path, auth=basicAuth).text
+    soup = BeautifulSoup(source, 'lxml')
+    for docLink in soup.find_all('a', onfocus="OnLink(this)", href=True):
+        href = docLink.get('href')
+        try:
+            print(urllib.parse.unquote(href))
+            if 'AllItems.aspx' in href:
+                download_folder(href, basic_auth)
+            else:
+                download_file(href, basic_auth)
+        except:
+            print(href, file=sys.stderr)
+    next = soup.find('img', alt='Next')
+    if next is not None:
+        nextLink = next.find_parent('a')
+        link = re.search('"(.*)"',nextLink.get('onclick'))
+        download_folder(json.loads(link.group()),basic_auth)
+
+download_folder(baseUri, basicAuth)
